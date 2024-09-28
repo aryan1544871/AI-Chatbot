15812a3f83795ce21114c455cde0591b10b9150e
Aryan Kushwaha <aryan.mnitj@gmail.com>
2024-09-28 16:24:31 +0530

    front end pretty much done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello AI Chatbot</p>
+    <div className="app">
+     <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className = "history">
+          <li>Aryan</li>
+          <li>Kumar</li>
+        </ul>
+        <nav>
+          <p>Made by Aryan</p>
+        </nav>
+     </section>
+     <section className = "main">
+      <h1>AI-Chatbot</h1>
+      <ul className = "feed"></ul>
+      <div className = "bottom-container">
+        <div className = "input-container">
+          <input/>
+          <div id = "submit">➢</div>
+        </div>
+        <p className="info">By messaging AI-Chatbot, you agree to our Terms and have read our Privacy Policy.</p>
+      </div>
+     </section>
+
     </div>
   );
 }
