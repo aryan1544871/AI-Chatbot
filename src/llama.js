04c416c1ebebd457ddcb8524758679b1692ee14a
Aryan <ak1544871@gmail.com>
2025-10-02 11:59:08 +0530

    changed llama function

--- src/llama.js
+++ src/llama.js
@@ -1,22 +1,22 @@
-const Groq = require('groq-sdk');
-const groq = new Groq({
-    apiKey: process.env.REACT_APP_KEY ,dangerouslyAllowBrowser: true
-  });
 
 export async function llamaModel(prompt) {
-  const chatCompletion = await groq.chat.completions.create({
+
+  var response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+  method: "POST",
+  headers: {
+    "Authorization": "Bearer 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",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    "model": "meta-llama/llama-3.3-8b-instruct:free",
     "messages": [
       {
         "role": "user",
         "content": prompt
       }
-    ],
-    "model": "llama3-8b-8192",
-    "temperature": 1,
-    "max_tokens": 1024,
-    "top_p": 1,
-    "stop": null
-  });
-  return chatCompletion;
-
+    ]
+  })
+});
+var chatCompletion = await response.json();
+return chatCompletion;
 }
