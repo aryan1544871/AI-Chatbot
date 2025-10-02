4b7373068cef1bdb99bf4735dd706fdd77618d65
Aryan <ak1544871@gmail.com>
2025-10-02 12:35:53 +0530

    changes done in llama function

--- src/llama.js
+++ src/llama.js
@@ -1,22 +1,24 @@
 
+import fetch from 'node-fetch';
+
 export async function llamaModel(prompt) {
 
-  var response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
-  method: "POST",
-  headers: {
-    "Authorization": "Bearer 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",
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    "model": "meta-llama/llama-3.3-8b-instruct:free",
-    "messages": [
-      {
-        "role": "user",
-        "content": prompt
-      }
-    ]
-  })
-});
-var chatCompletion = await response.json();
-return chatCompletion;
+  const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${process.env.REACT_APP_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "model": "meta-llama/llama-3.3-8b-instruct:free",
+      "messages": [
+        {
+          "role": "user",
+          "content": prompt
+        }
+      ]
+    })
+  });
+  const chatCompletion = await response.json();
+  return chatCompletion;
 }
